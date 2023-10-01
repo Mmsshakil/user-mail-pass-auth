205edd9525a99b0724f715f0f1d2e941294a48f4
Mmsshakil <softhandshakil@gmail.com>
2023-10-02 01:56:50 +0600

    registration form design done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,16 @@
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register here</h2>
+        <div className="text-center border">
+            <h2 className="text-2xl">Register here</h2>
+
+            <form className="mt-3 text-center">
+                <input className="p-1 mb-2" type="email" name="email" id="" placeholder="Email..." />
+                <br />
+                <input className="p-1 mb-2" type="password" name="password" id="" placeholder="Password..." />
+                <br />
+                <input className="btn btn-primary mb-3" type="submit" value="Register" />
+            </form>
         </div>
     );
 };
